1e2dcd59a5127f89a7f32357a3a2946bf3275bc1
Kramerushka <email@example.rupainandlove95@gmail.com>
2023-12-21 10:47:49 +0600

    Задание 8.1.4

--- Program.cs
+++ Program.cs
@@ -1,10 +1,31 @@
 ﻿namespace Module8
 {
+    /// <summary>
+    /// Задание 8.1.4
+    /// </summary>
+    /// Придумайте простой класс, который будет предоставлять информацию об установленном в системе диске.
+    /// Нужны свойства, чтобы хранить: имя диска , объём, свободное место. 
+    /// Свойства инициализируются при создании нового объекта в методе-конструкторе.
+    /// 
+    class Drive
+    {
+        public Drive(string name, long totalSpace, long freeSpace)
+        {
+            Name = name;
+            TotalSpace = totalSpace;
+            FreeSpace = freeSpace;
+        }
+
+        public string Name { get; }
+        public long TotalSpace { get; }
+        public long FreeSpace { get; }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
         }
     }
 }
