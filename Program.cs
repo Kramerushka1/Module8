cc90864084c29a11ce4d8f0ea9c695cb381335cd
Kramerushka <email@example.rupainandlove95@gmail.com>
2023-12-21 23:13:30 +0600

    Задание 8.2.2

--- Program.cs
+++ Program.cs
@@ -1,55 +1,76 @@
-﻿    using System;
-    using System;
-    using System.IO;
+﻿using System;
+using System;
+using System.IO;
 
-    namespace Module8
+namespace Module8
+{
+    class Program
     {
-        class Program
+        static void Main(string[] args)
         {
-            static void Main(string[] args)
+            GetCatalogs();
+            
+            Console.WriteLine();
+            GetCatalogsCount();
+            
+            Console.WriteLine();
+            CreateNewFolder();
+
+            Console.WriteLine();
+            GetCatalogs();
+
+            Console.WriteLine();
+            GetCatalogsCount();
+        }
+
+        static void GetCatalogs()
+        {
+            string dirName = @"E:\Sample Folder\"; // Прописываем путь к корневой директории MacOS (для Windows скорее всего тут будет "C:\\")
+            if (Directory.Exists(dirName)) // Проверим, что директория существует
             {
-                GetCatalogs(); //   Вызов метода получения директорий
+                Console.WriteLine("Папки:");
+                string[] dirs = Directory.GetDirectories(dirName);  // Получим все директории корневого каталога
+
+                foreach (string d in dirs) // Выведем их все
+
+                {
+                    Console.WriteLine(d);
+                }
+
                 Console.WriteLine();
-                GetCatalogsCount();
-            }
+                Console.WriteLine("Файлы:");
+                string[] files = Directory.GetFiles(dirName);// Получим все файлы корневого каталога
 
-            static void GetCatalogs()
-            {
-                string dirName = @"C:\\"; // Прописываем путь к корневой директории MacOS (для Windows скорее всего тут будет "C:\\")
-                if (Directory.Exists(dirName)) // Проверим, что директория существует
+                foreach (string s in files)   // Выведем их все
                 {
-                    Console.WriteLine("Папки:");
-                    string[] dirs = Directory.GetDirectories(dirName);  // Получим все директории корневого каталога
-
-                    foreach (string d in dirs) // Выведем их все
-
-                    {
-                        Console.WriteLine(d);
-                    }
-
-                    Console.WriteLine();
-                    Console.WriteLine("Файлы:");
-                    string[] files = Directory.GetFiles(dirName);// Получим все файлы корневого каталога
-
-                    foreach (string s in files)   // Выведем их все
-                        Console.WriteLine(s);
-                }
-            }
-
-            static void GetCatalogsCount()
-            {
-                try
-                {
-                    DirectoryInfo dirInfo = new DirectoryInfo(@"C:\\");
-                    if (dirInfo.Exists)
-                    {
-                        Console.WriteLine($"Количество файлов и папок {dirInfo.GetDirectories().Length + dirInfo.GetFiles().Length}");
-                    }
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e.Message);
+                    Console.WriteLine(s);
                 }
             }
         }
+        static void GetCatalogsCount()
+        {
+            try
+            {
+                DirectoryInfo dirInfo = new DirectoryInfo(@"E:\Sample Folder\");
+                if (dirInfo.Exists)
+                {
+                    Console.WriteLine($"Количество файлов и папок {dirInfo.GetDirectories().Length + dirInfo.GetFiles().Length}");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        static void CreateNewFolder()
+        {
+            DirectoryInfo dirInfo = new DirectoryInfo(@"E:\Sample Folder\");
+            if (!dirInfo.Exists)
+            {
+                dirInfo.Create();
+            }
+
+            dirInfo.CreateSubdirectory("NewFolder");
+        }
     }
+}
