711ff5900c5d292ef8aed9271c7920283f7e99b3
Kramerushka <email@example.rupainandlove95@gmail.com>
2023-12-21 23:37:34 +0600

    Задание 8.2.3

--- Program.cs
+++ Program.cs
@@ -10,28 +10,30 @@ namespace Module8
         {
             GetCatalogs();
             
-            Console.WriteLine();
             GetCatalogsCount();
             
-            Console.WriteLine();
             CreateNewFolder();
 
-            Console.WriteLine();
             GetCatalogs();
 
-            Console.WriteLine();
+            GetCatalogsCount();
+
+            DeleteNewFolder();
+
+            GetCatalogs();
+
             GetCatalogsCount();
         }
 
         static void GetCatalogs()
         {
-            string dirName = @"E:\Sample Folder\"; // Прописываем путь к корневой директории MacOS (для Windows скорее всего тут будет "C:\\")
-            if (Directory.Exists(dirName)) // Проверим, что директория существует
+            string dirName = @"E:\Sample Folder\";  // Прописываем путь к директории
+            if (Directory.Exists(dirName))  // Проверим, что директория существует
             {
                 Console.WriteLine("Папки:");
-                string[] dirs = Directory.GetDirectories(dirName);  // Получим все директории корневого каталога
+                string[] dirs = Directory.GetDirectories(dirName);  // Получим все директории каталога
 
-                foreach (string d in dirs) // Выведем их все
+                foreach (string d in dirs)  // Выведем их все
 
                 {
                     Console.WriteLine(d);
@@ -39,28 +41,30 @@ namespace Module8
 
                 Console.WriteLine();
                 Console.WriteLine("Файлы:");
-                string[] files = Directory.GetFiles(dirName);// Получим все файлы корневого каталога
+                string[] files = Directory.GetFiles(dirName);   // Получим все файлы корневого каталога
 
                 foreach (string s in files)   // Выведем их все
                 {
                     Console.WriteLine(s);
                 }
             }
+            Console.WriteLine();
         }
         static void GetCatalogsCount()
         {
             try
             {
                 DirectoryInfo dirInfo = new DirectoryInfo(@"E:\Sample Folder\");
-                if (dirInfo.Exists)
+                if (dirInfo.Exists) //Проверяем наличие дирректории
                 {
-                    Console.WriteLine($"Количество файлов и папок {dirInfo.GetDirectories().Length + dirInfo.GetFiles().Length}");
+                    Console.WriteLine($"Количество файлов и папок {dirInfo.GetDirectories().Length + dirInfo.GetFiles().Length}"); //Складываем количество файлов м папок
                 }
             }
             catch (Exception e)
             {
                 Console.WriteLine(e.Message);
             }
+            Console.WriteLine();
         }
         static void CreateNewFolder()
         {
@@ -71,6 +75,23 @@ namespace Module8
             }
 
             dirInfo.CreateSubdirectory("NewFolder");
+            Console.WriteLine("Папка 'NewFolder' создана");
+
+            Console.WriteLine();
+        }
+        static void DeleteNewFolder()
+        {
+            try
+            {
+                DirectoryInfo dirInfo = new DirectoryInfo(@"E:\Sample Folder\NewFolder");
+                dirInfo.Delete(true);
+                Console.WriteLine("Папка 'NewFolder' удалена");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.WriteLine();
         }
     }
 }
