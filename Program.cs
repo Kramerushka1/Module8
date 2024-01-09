2ccc859c80cfef8750824a766e33ad1f42f77488
Kramerushka <email@example.rupainandlove95@gmail.com>
2024-01-09 12:19:05 +0600

    Задание 8.2.3

--- Program.cs
+++ Program.cs
@@ -1,92 +1,78 @@
-﻿using System;
-using System;
-using System.IO;
-
-namespace Module8
+﻿namespace Module8
 {
-    class Program
+    /// <summary>
+    /// Задание 8.2.3
+    /// </summary>
+    /// 
+    ///Добавьте в задание 8.2.2 удаление вновь созданной директории и проверьте: 
+    ///теперь ваша программа не должна оставлять после себя следов!
+
+    class Count
     {
-        static void Main(string[] args)
-        {
-            GetCatalogs();
-            GetCatalogsCount();
-            
-            CreateNewFolder();
-
-            GetCatalogs();
-            GetCatalogsCount();
-
-            DeleteNewFolder();
-
-            GetCatalogs();
-            GetCatalogsCount();
-        }
-
-        static void GetCatalogs()
-        {
-            string dirName = @"E:\Sample Folder\"; // Прописываем путь к корневой директории MacOS (для Windows скорее всего тут будет "C:\\")
-            if (Directory.Exists(dirName)) // Проверим, что директория существует
-            {
-                Console.WriteLine("Папки:");
-                string[] dirs = Directory.GetDirectories(dirName);  // Получим все директории корневого каталога
-
-                foreach (string d in dirs) // Выведем их все
-
-                {
-                    Console.WriteLine(d);
-                }
-
-                Console.WriteLine();
-                Console.WriteLine("Файлы:");
-                string[] files = Directory.GetFiles(dirName);// Получим все файлы корневого каталога
-
-                foreach (string s in files)   // Выведем их все
-                {
-                    Console.WriteLine(s);
-                }
-                Console.WriteLine();
-            }
-        }
-        static void GetCatalogsCount()
+        public static void FilesAndFolders()
         {
             try
             {
-                DirectoryInfo dirInfo = new DirectoryInfo(@"E:\Sample Folder\");
-                if (dirInfo.Exists)
+                DirectoryInfo dir = new DirectoryInfo(@"C:\\");
+                if (dir.Exists)
                 {
-                    Console.WriteLine($"Количество файлов и папок {dirInfo.GetDirectories().Length + dirInfo.GetFiles().Length}");
+                    Console.WriteLine($"В дирректории {dir.GetDirectories().Length + dir.GetFiles().Length} файлов и папок");
                 }
             }
             catch (Exception e)
             {
                 Console.WriteLine(e.Message);
             }
-            Console.WriteLine();
         }
-        static void CreateNewFolder()
-        {
-            DirectoryInfo dirInfo = new DirectoryInfo(@"E:\Sample Folder\");
-            if (!dirInfo.Exists)
-            {
-                dirInfo.Create();
-            }
-
-            dirInfo.CreateSubdirectory("New Folder");
-            Console.WriteLine("Папка 'New Folder' создана");
-            Console.WriteLine();
-        }
-        static void DeleteNewFolder()
+    }
+    class Dir
+    {
+        public static void Create()
         {
             try
             {
-                DirectoryInfo dirInfo = new DirectoryInfo(@"E:\Sample Folder\New Folder");
-                dirInfo.Delete(true);
-                Console.WriteLine("Папка 'New Folder' удалена");
+                DirectoryInfo dir = new DirectoryInfo(@"C:\\newDirectory");
+                if (!dir.Exists)
+                {
+                    dir.Create();
+                    Console.WriteLine("Папка newDirectory создана");
+                }
             }
-            catch (Exception ex)
+            catch (Exception e)
             {
-                Console.WriteLine(ex.Message);
+                Console.WriteLine(e.Message);
             }
+        }
+        public static void Delete()
+        {
+            try
+            {
+                DirectoryInfo dir = new DirectoryInfo(@"C:\\newDirectory");
+                if (dir.Exists)
+                {
+                    dir.Delete();
+                    Console.WriteLine("Папка newDirectory удалена");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Count.FilesAndFolders();
+            Console.WriteLine();
+
+            Dir.Create();
+            Count.FilesAndFolders();
+            Console.WriteLine();
+
+            Dir.Delete();
+            Count.FilesAndFolders();
             Console.WriteLine();
         }
     }
