68ffdcf97c3e67f65bcf31b414a3d0dd0a712576
Kramerushka <email@example.rupainandlove95@gmail.com>
2024-01-09 17:11:00 +0600

    Задание 8.3.2

--- Program.cs
+++ Program.cs
@@ -4,45 +4,39 @@ using System.Linq;
 
 namespace Module8
 {
+    /// <summary>
+    /// Задание 8.3.2
+    /// </summary>
+    /// Сделайте так, чтобы ваша программа из задания 8.3.1 
+    /// при каждом запуске добавляла в свой исходный код комментарий 
+    /// о времени последнего запуска. 
+
     class Program
     {
         static void Main(string[] args)
         {
-            string tempFile = Path.GetTempFileName();
-            var fileInfo = new FileInfo(tempFile);
+            string filePath = @"E:\Skillfactory Modules\Module8\Module8\Program.cs";
+            FileInfo fileInfo = new FileInfo(filePath);
 
-            using (StreamWriter sw = fileInfo.CreateText())
+
+            using (StreamWriter sw = fileInfo.AppendText()) 
             {
-                sw.WriteLine("Саша");
-                sw.WriteLine("Паша");
-                sw.WriteLine("Леша");
+                sw.WriteLine($"///Последний запуск: {DateTime.Now}");
             }
 
-            using (StreamReader sr = fileInfo.OpenText())
+            using (StreamReader sr = File.OpenText(filePath))
             {
                 string str = "";
                 while ((str = sr.ReadLine()) != null)
                 {
                     Console.WriteLine(str);
                 }
-            }
+                
 
-            try
-            {
-                string tempFile2 = Path.GetTempFileName();
-                var fileInfo2 = new FileInfo(tempFile2);
-
-                fileInfo2.Delete();
-
-                fileInfo.CopyTo(tempFile2);
-                Console.WriteLine($"файл {tempFile} скопирован в файл {tempFile2}");
-                fileInfo.Delete();
-                Console.WriteLine($"файл {tempFile} удален");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Ошибка: {ex.ToString()}");
             }
         }
     }
 }
+///Последний запуск: 09.01.2024 17:10:40
+///Последний запуск: 09.01.2024 17:10:44
+///Последний запуск: 09.01.2024 17:10:51
