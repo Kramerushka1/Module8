2d5b4e0e0c38b83c0af0997c06e98353f2283a07
Kramerushka <email@example.rupainandlove95@gmail.com>
2024-01-09 13:52:00 +0600

    Задание 8.2.4

--- Program.cs
+++ Program.cs
@@ -1,19 +1,21 @@
-﻿namespace Module8
+﻿using Microsoft.VisualBasic.FileIO;
+
+namespace Module8
 {
     /// <summary>
-    /// Задание 8.2.3
+    /// Задание 8.2.4
     /// </summary>
     /// 
-    ///Добавьте в задание 8.2.2 удаление вновь созданной директории и проверьте: 
-    ///теперь ваша программа не должна оставлять после себя следов!
+    ///Создайте на рабочем столе папку testFolder. 
+    ///Напишите метод, с помощью которого можно будет переместить её в корзину.
 
     class Count
     {
-        public static void FilesAndFolders()
+        public static void FilesAndFolders(string dirCount)
         {
             try
             {
-                DirectoryInfo dir = new DirectoryInfo(@"C:\\");
+                DirectoryInfo dir = new DirectoryInfo(@dirCount);
                 if (dir.Exists)
                 {
                     Console.WriteLine($"В дирректории {dir.GetDirectories().Length + dir.GetFiles().Length} файлов и папок");
@@ -21,42 +23,58 @@
             }
             catch (Exception e)
             {
-                Console.WriteLine(e.Message);
+                Console.WriteLine($"Ошибка при подсчете файлов и папок: {e.Message}");
             }
         }
     }
     class Dir
     {
-        public static void Create()
+        public static void Create(string dirCreate)
         {
             try
             {
-                DirectoryInfo dir = new DirectoryInfo(@"C:\\newDirectory");
+                DirectoryInfo dir = new DirectoryInfo(@dirCreate);
                 if (!dir.Exists)
                 {
                     dir.Create();
-                    Console.WriteLine("Папка newDirectory создана");
+                    Console.WriteLine("Папка testFolder создана");
                 }
             }
             catch (Exception e)
             {
-                Console.WriteLine(e.Message);
+                Console.WriteLine($"Ошибка при создании папки: {e.Message}");
             }
         }
-        public static void Delete()
+        public static void Delete(string dirDelete)
         {
             try
             {
-                DirectoryInfo dir = new DirectoryInfo(@"C:\\newDirectory");
+                DirectoryInfo dir = new DirectoryInfo(@dirDelete);
                 if (dir.Exists)
                 {
                     dir.Delete();
-                    Console.WriteLine("Папка newDirectory удалена");
+                    Console.WriteLine("Папка testFolder удалена");
                 }
             }
             catch (Exception e)
             {
-                Console.WriteLine(e.Message);
+                Console.WriteLine($"Ошибка при удалении папки: {e.Message}");
+            }
+        }
+        public static void DeleteDirectory(string dirDelete)
+        {
+            try
+            {
+                FileSystem.DeleteDirectory(
+                    dirDelete, 
+                    UIOption.AllDialogs, 
+                    RecycleOption.SendToRecycleBin, 
+                    UICancelOption.ThrowException);
+                Console.WriteLine("Папка testFolder удалена в корзину.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка при удалении папки в корзину: {ex.Message}");
             }
         }
     }
@@ -64,15 +82,26 @@
     {
         static void Main(string[] args)
         {
-            Count.FilesAndFolders();
+            string dirCount = "C:\\Users\\mrdoo\\desktop";
+            string newDir = "C:\\Users\\mrdoo\\desktop\\testFolder";
+
+            Count.FilesAndFolders(dirCount);
             Console.WriteLine();
 
-            Dir.Create();
-            Count.FilesAndFolders();
+            Dir.Create(newDir);
+            Count.FilesAndFolders(dirCount);
             Console.WriteLine();
 
-            Dir.Delete();
-            Count.FilesAndFolders();
+            Dir.Delete(newDir);
+            Count.FilesAndFolders(dirCount);
+            Console.WriteLine();
+
+            Dir.Create(newDir);
+            Count.FilesAndFolders(dirCount);
+            Console.WriteLine();
+
+            Dir.DeleteDirectory(newDir);
+            Count.FilesAndFolders(dirCount);
             Console.WriteLine();
         }
     }
