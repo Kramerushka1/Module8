8e921d382e7637755fe0fe3370fffddc1fc300ed
Kramerushka <email@example.rupainandlove95@gmail.com>
2024-01-11 15:59:13 +0600

    Сериализация

--- Program.cs
+++ Program.cs
@@ -5,51 +5,21 @@ using System.Reflection.PortableExecutable;
 
 namespace Module8
 {
-    /// <summary>
-    /// Задание 8.4.2
-    /// </summary>
-    /// Запишите  в файл из предыдущего задания информацию о доступе к нему с вашей машины. 
-    /// Пример вывода, который должен получиться: 
-    /// Файл изменен 02.11 14:53 на компьютере Windows 11
-
     class Program
     {
-        const string filePath = "C:\\Users\\mrdoo\\Desktop\\BinaryFile.bin";
+        
+    }
+    
+    [Serializable]
+    class Person
+    {
+        public string Name { get; set; }
+        public int Year { get; set; }
 
-        static void Main()
+        public Person(string name, int year)
         {
-            WriteFile();
-            ReadFile();
-        }
-
-        static void WriteFile()
-        {
-            string StringValue;
-
-            if (File.Exists(filePath))
-            {
-                using (BinaryWriter writer = new BinaryWriter(File.Open(filePath, FileMode.Open)))
-                {
-                    writer.Write($"Файл изменен {DateTime.Now} на компьютере {Environment.OSVersion}");
-                }
-            }
-        }
-
-        static void ReadFile()
-        {
-            string StringValue;
-
-            if (File.Exists(filePath))
-            {
-                using (BinaryReader reader = new BinaryReader(File.Open(filePath, FileMode.Open)))
-                {
-                    StringValue = reader.ReadString();
-                }
-
-                Console.WriteLine("Из файла считано");
-
-                Console.WriteLine($"{StringValue}");
-            }
+            Name = name;
+            Year = year;
         }
     }
 }
